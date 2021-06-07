7f9cda58d631a672d7987891ec1c30d149e8c30f
iOS开发 <85385393+uzi-yyds-code@users.noreply.github.com>
2021-06-07 16:15:40 +0800

    Create iOS面试--头条最新iOS开发面试题.m

---
+++ iOS面试--头条最新iOS开发面试题.m
@@ -0,0 +1,129 @@
+关于面试题，可能没那么多时间来总结答案，有什么需要讨论的地方欢迎大家指教。主要记录一下准备过程，和面试的一些总结，希望能帮助到正在面试或者将要面试的同学吧。
+
+##头条
+
+#####一面
+
+* 1、自我介绍
+
+* 2、项目相关
+
+* 3、怎么自定义导航跳转
+
+* 4、谈谈runtime的理解
+
+* 5、KVC的用途
+
+* 6、使用method swizzling要注意什么？（进行版本迭代的时候需要进行一些检验，防止系统库的函数发生了变化）
+
+* 7、谈对引用计数的理解
+
+* 8、谈谈runloop的理解
+
+* 9、runloop有哪些状态
+
+* 10、autoreleasepool的使用场景
+
+* 11、TableView优化，怎么减少卡顿
+
+* 12、copy assign retain weak关键词
+
+* 13、JSON转Model
+
+* 代码布局
+
+* 多屏幕适配
+
+* HTTP请求头和响应头
+
+* Cookie
+
+* NSCache
+
+* 自己设计一个缓存器
+
+* 怎么实现LRU
+
+* SDWebImage
+
+* 二叉树先序遍历（递归和非递归）
+
+####二面
+
+* MVC的一些缺点
+
+* 讲一讲其它架构
+
+* 你知道哪些编码方式
+
+* 算法字符串翻转
+
+* 多线程的方式和它们的区别
+
+* 队列和线程的关系
+
+* 一道线程安全的题
+
+* 有哪些锁
+
+* 属性的关键字
+
+* assign可以用于OC对象吗
+
+* copy和strong的区别
+
+* weak如何实现自动赋nil
+
+* 为什么不可变对象要用copy
+
+* assing可以使用在对象中吗
+
+* Pod update和pod install的区别
+
+* layoutIfNeeded和setNeedsLayout的区别
+
+* 抓包工具抓取HTTPS的原理
+
+* isEquel和hash的关系
+
+* bitmap的结构
+
+* 可变数组的实现原理
+
+* 如何hook一个对象的方法，而不影响其它对象
+
+* 如何避免if else
+
+* 自旋锁和互斥锁的区别
+
+####三面
+
+* 介绍项目，主要介绍自己强项一点的地方
+
+* 数组cop后里面的元素会复制一份新的吗
+
+* 数组的浅拷贝与深拷贝
+
+* TCP为什么是三次握手和四次挥手
+
+头条一直都是视频面，而且是一条龙服务。总体来说感觉不错，反正主要就是需要基础足够扎实；
+###### 精选全网 · iOS面试题答案PDF文集
+
+> *   最新 初级iOS 面试题
+> *   最新 中级iOS 面试题
+> *   最新 高级iOS 面试题
+> *   《BAT面试资料全集》
+> *   《BAT大厂常问iOS面试题》
+> *   《2021年面试真题》
+> *   《iOS中级到高级面试题完整版》
+> *   字节跳动，京东，小米，腾讯、头条、阿里、美团等公司21年的面试真题
+
+![image](https://upload-images.jianshu.io/upload_images/18671920-488f6659d2be275e.png?imageMogr2/auto-orient/strip|imageView2/2/w/867/format/webp)
+
+资料下载地址：
+
+> 获取地址：[https://docs.qq.com/doc/DVU5LY1Bsb3dSZ3Nn](https://links.jianshu.com/go?to=https%3A%2F%2Fdocs.qq.com%2Fdoc%2FDVU5LY1Bsb3dSZ3Nn)
+
+
+
+
